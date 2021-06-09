87440a444f34cf95c71025910cc9add3b4724667
Roberto <50220857+robertCrtz@users.noreply.github.com>
2021-06-09 16:28:27 -0600

    Update pag_inicio.php

--- vistas/pag_inicio.php
+++ vistas/pag_inicio.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if($_SESSION["s_usuario"] === null){
+if($_SESSION["usuario"] === null){
     header("Location: ../index.php");
 }
 
@@ -15,7 +15,7 @@ if($_SESSION["s_usuario"] === null){
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Login con  PHP - Bootstrap 4</title>
 
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../estilos/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="../estilos.css">
         <link rel="stylesheet" href="../plugins/sweetalert2/sweetalert2.min.css">            
     </head>    
@@ -27,7 +27,7 @@ if($_SESSION["s_usuario"] === null){
                     <div class="jumbotron">
                         
                         <h1 class="display-4 text-center">¡Bienvenido!</h1>
-                      <h2 class="text-center">Usuario: <span class="badge badge-primary"><?php echo $_SESSION["s_usuario"];?></span></h2>    
+                      <h2 class="text-center">Usuario: <span class="badge badge-primary"><?php echo $_SESSION["usuario"];?></span></h2>    
                       <p class="lead text-center">Esta es la página de inicio, luego de un LOGIN correcto.</p>
                       <hr class="my-4">          
                       <a class="btn btn-danger btn-lg" href="../bd/logout.php" role="button">Cerrar Sesión</a>
@@ -36,7 +36,7 @@ if($_SESSION["s_usuario"] === null){
             </div>
         </div>        
      <script src="../jquery/jquery-3.3.1.min.js"></script>    
-     <script src="../bootstrap/js/bootstrap.min.js"></script>    
+     <script src="../estilos/bootstrap/js/bootstrap.min.js"></script>    
      <script src="../popper/popper.min.js"></script>    
         
      <script src="../plugins/sweetalert2/sweetalert2.all.min.js"></script>    
