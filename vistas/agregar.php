82dde179947a4a4aa23934df8023b1fcf68652df
Roberto <50220857+robertCrtz@users.noreply.github.com>
2021-06-21 16:42:16 -0600

    Add files via upload

--- vistas/agregar.php
+++ vistas/agregar.php
@@ -20,21 +20,20 @@ if($_SESSION["usuario"] === null){
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../estilos.css">
         <link rel="stylesheet" href="vista.css">
-        <link rel="stylesheet" href="../plugins/sweetalert2/sweetalert2.min.css">
-        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">            
+        <link rel="stylesheet" href="../plugins/sweetalert2/sweetalert2.min.css">            
     </head>    
     <body>
         <header>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">CONNA</a>
+                    <a class="navbar-brand" href="pag_inicio.php">CONNA</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="agregar.php">Agregar caso</a>
+                                <a class="nav-link" aria-current="page" href="#">Agregar caso</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Link</a>
@@ -48,14 +47,35 @@ if($_SESSION["usuario"] === null){
             </nav>
         </header>
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="jumbotron">
-                        <h1 class="text-center">¡Bienvenido!</h1>
-                      <h2 class="text-center"><span class=""><?php echo $_SESSION["usuario"];?></span></h2>             
-                    </div>
-                </div>
-            </div>
+
+        <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="text" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="text" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="text" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="text" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
         </div>        
      <script src="../jquery/jquery-3.3.1.min.js"></script>
      <script src="../estilos/popper/popper.min.js"></script>    
