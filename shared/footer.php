270297fc53aabc3298f1c8e6643a22cd79823bb2
Roberto <50220857+robertCrtz@users.noreply.github.com>
2021-06-23 10:43:42 -0600

    Add files via upload

---
+++ shared/footer.php
@@ -0,0 +1,11 @@
+
+<footer>
+    <div class="container-footer-all mt-5 bg-dark text-white">
+
+        <div class="container-body text-center">
+
+                <h3 class="text-white mt-5">Control de Acogimientos de Emergencias</h3>
+                &copy; 2021 CONNA
+        </div>
+    </div>
+</footer>
---
+++ shared/navbar.php
@@ -0,0 +1,11 @@
+
+            <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">Configuración</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="" href="../bd/logout.php" role="button">Cerrar Sesión</a>
+                    </div>
+                </li>
+            </ul>
