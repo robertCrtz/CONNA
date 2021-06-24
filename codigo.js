829ed7e8025ddbe572e17061832ff88bcce426f7
Roberto <50220857+robertCrtz@users.noreply.github.com>
2021-06-24 15:49:41 -0600

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -33,3 +33,13 @@ $(document).ready(function(){
         $("body").toggleClass("sb-sidenav-toggled");
     });
 })(jQuery);
+
+$(document).ready(function (){
+    $('#busqueda').keyup(function (){
+        var resultado = new RegExp($(this).val());
+        $('.busquedatabla tr').hide();
+        $('.busquedatabla tr').filter(function (){
+            return resultado.test($(this).text());
+        }).show();
+    })
+});
