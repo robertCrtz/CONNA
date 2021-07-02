0aa17358a326bb1e78f20a6d823b9d547f6cd3b1
Roberto <50220857+robertCrtz@users.noreply.github.com>
2021-07-02 15:18:26 -0600

    Add files via upload

--- shared/sidebar.php
+++ shared/sidebar.php
@@ -11,14 +11,14 @@
 
                 <?php //if($id_rol == 2) { ?>
                 <div class="sb-sidenav-menu-heading">Control</div>
-                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                        <div class="sb-nav-link-icon">
-                            <i class="fas fa-book-open"></i>
-                        </div>Casos
-                        <div class="sb-sidenav-collapse-arrow">
-                            <i class="fas fa-angle-down"></i>
-                        </div>
-                    </a>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-book-open"></i>
+                    </div>Casos
+                    <div class="sb-sidenav-collapse-arrow">
+                        <i class="fas fa-angle-down"></i>
+                    </div>
+                </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
                         <a class="nav-link" href="../vistas/agregar.php">Agregar Caso</a>
@@ -26,6 +26,20 @@
                     </nav>
                 </div>
                 <?php// } ?>
+
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAcogimiento" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-home"></i>
+                    </div>Acogimiento
+                    <div class="sb-sidenav-collapse-arrow">
+                        <i class="fas fa-angle-down"></i>
+                    </div>
+                </a>
+                <div class="collapse" id="collapseAcogimiento" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="../vistas/tipoAcogimiento.php">Tipos de acogimiento</a>
+                    </nav>
+                </div>
                 
 
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsers" aria-expanded="false" aria-controls="collapseUsers">
