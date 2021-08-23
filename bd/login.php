bc97a90194f21f2bb482754ffc65b4798ba36fef
Roberto <50220857+robertCrtz@users.noreply.github.com>
2021-08-23 08:13:35 -0600

    Add files via upload

--- bd/login.php
+++ bd/login.php
@@ -1,21 +1,22 @@
 <?php
-include 'conexion.php';
+  include 'conexion.php';
 
-$usuario=$_POST['usuario'];
-$contrasena=$_POST['contrasena'];
-session_start();
-$_SESSION['usuario']=$usuario;
+  $usuario=$_POST['usuario'];
+  $contrasena=$_POST['contrasena'];
+  session_start();
+  $_SESSION['usuario']=$usuario;
+  $_SESSION['id_rol']=$id_rol;
 
-$consulta="SELECT*FROM usuarios WHERE usuario='$usuario' and contrasena='$contrasena'";
-$resultado=mysqli_query($con,$consulta);
+  $consulta="SELECT*FROM usuarios WHERE usuario='$usuario' and contrasena='$contrasena'";
+  $resultado=mysqli_query($con,$consulta);
 
-$filas=mysqli_num_rows($resultado);
+  $filas=mysqli_num_rows($resultado);
 
-if($filas){
-    header("location:../vistas/pag_inicio.php");
-}else{
-    ?>
-  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  if($filas){
+      header("location:../vistas/pag_inicio.php");
+  }else{
+  ?>
+    <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
   <?php
 }
 mysqli_free_result($resultado);
